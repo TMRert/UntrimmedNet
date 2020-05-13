ad5f1eab143d522257a83339d31ff60e75bf2584
Tim Rietveld <timr2808@gmail.com>
2020-05-13 13:53:43 +0200

    fix error in model definition and script weight location

--- scripts/train_mementos.sh
+++ scripts/train_mementos.sh
@@ -3,7 +3,7 @@
 TOOLS=/opt/caffe/build/tools
 
 /usr/bin/mpirun -n 1 \
-$TOOLS/caffe train --solver=../models/mementos_untrimmednet_solver.prototxt --weights=../models/bn_inception_flow_init.caffemodel
+$TOOLS/caffe train --solver=../models/mementos_untrimmednet_solver.prototxt --weights=../models/anet1.2_temporal_untrimmednet_soft_bn_inception.caffemodel
 
 echo "Done."
 
